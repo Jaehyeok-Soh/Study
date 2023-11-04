aec7ace923fd08c77916b730bb4e7685c4423aee
study <revol3154@gmail.com>
2023-11-04 14:36:51 +0800

    first commit

---
+++ ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using ChatService.Model;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatService.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _botUser;
+
+        public ChatHub()
+        {
+            _botUser = "MyCheat Bot";
+        }
+
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", _botUser,
+                $"{userConnection.User} has joined {userConnection.Room}");
+        }
+    }
+}
---
+++ ChatService/Model/UserConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatService.Model
+{
+    public class UserConnection
+    {
+        public string User { get; set; }
+        public string Room { get; set; }
+    }
+}
